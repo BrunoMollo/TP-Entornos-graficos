7b5046f2f5e437393adaca0eb7107c3b06baefc0
KevinMasci <kevin-sor@hotmail.com>
2024-01-30 18:24:53 -0300

    drive credentials

--- modulo-vacantes/config/services.php
+++ modulo-vacantes/config/services.php
@@ -31,4 +31,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_DRIVE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
+        'refresh_token' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
+        'folder-id' => env('GOOGLE_DRIVE_FOLDER-ID'),
+    ],
+
 ];
